90fbd03db051e266b6cc29aa6b5e4dae2f2fc9d0
cyveros <cyveros@gmail.com>
2016-08-16 09:31:14 -0400

    fixes - use const for required package

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 var should = require('chai').should;
 var expect = require('chai').expect;
+
 var RSDN = require('../index')();
 var Namespace = RSDN.namespace;
 var Store = RSDN.store;
@@ -11,8 +12,8 @@ describe('#rsdn', function(){
 		var ns = new Namespace();
 
 		expect(ns).to.be.instanceof(Namespace);
-		
 		expect(ns.getAll()).to.be.instanceof(Store);
+		
 		// (ns.debug() instanceof Store).should.equal(true);
 	});
 });
